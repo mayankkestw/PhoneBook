c22e788f93158a83fa6b0d3983627a2fede1911b
Mayank kestwal <mayankkestwal8@gmail.com>
2020-05-28 19:59:47 +0530

    pagination

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,37 +3,58 @@ var router = express.Router();
 const mongoose = require('mongoose');
 const Contact = mongoose.model('Contact');
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res, next) => {
     res.render("contact/addOrEditContact", {
-        viewTitle : "Add New Contact"
+        viewTitle: "Add New Contact"
     });
 });
 
-router.post('/', (req, res)=>{
-    insertContact(req, res);
+router.post('/', (req, res, next) => {
+    insertContact(req, res, next);
 });
 
-function insertContact(req, res){
+router.get('/list/:page', (req, res, next) => {
+    let perPage = 4;
+    let page = req.params.page || 1;
+
+    Contact
+        .find({}).lean()
+        .skip((perPage*page)-perPage)
+        .limit(perPage)
+        .exec((err, contacts)=>{
+            console.log(contacts);
+            Contact.count((err, count)=>{
+            if(err) return next(err);
+            res.render('contact/list', {
+                list:contacts,
+                current: page,
+                pages: Math.ceil(count/perPage)
+            });
+        });
+    });
+    
+});
+
+
+function insertContact(req, res, next) {
     var contact = new Contact();
     console.log('save running')
     contact.name = req.body.fullname;
     contact.email = req.body.email;
     contact.mobile = req.body.mobile;
     contact.birthday = req.body.birthday;
-    contact.save((err, doc)=>{
-        if(!err){
-            res.redirect('contact/list');
-        }else{
+    contact.save((err, doc) => {
+        if (!err) {
+            res.redirect('contact/list/1');
+        } else {
             if (err.name === 'MongoError' && err.code === 11000) {
                 return res.status(422).send({ success: false, message: 'User already exist!' });
-              }
-            console.log("Error during record insertion :"+ err)
+            }
+            console.log("Error during record insertion :" + err)
         }
     });
 }
 
-router.get('/list', (req, res)=>{
-    res.json('from list');
-});
+
 
 module.exports = router;
