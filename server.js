736dc35791751c3de4faff986f8c960797ec02cb
Mayank kestwal <mayankkestwal8@gmail.com>
2020-05-28 10:35:16 +0530

    controller added

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ require('./models/db');
 
 const express = require('express');
 
+const contactController = require('./controllers/contactController');
+
 var app = express();
 
 app.listen(3000, () => {
     console.log('Express server started at port : 3000');
 });
+
+app.use('/contact', contactController);
